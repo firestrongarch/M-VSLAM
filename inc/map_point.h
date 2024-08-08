a5a190591490964f8ec1d70f83f81ab1377cf6ba
fu <1766423721@qq.com>
2024-08-08 18:03:12 +0800

    mutex

--- inc/map_point.h
+++ inc/map_point.h
@@ -12,12 +12,12 @@ public:
     MapPoint(Eigen::Vector3d position);
 
     Eigen::Vector3d Pos(){
-        std::unique_lock<std::mutex> lck(update_get_mutex_);
+        std::unique_lock<std::mutex> lck(mutex_pos_);
         return *this;
     }
 
     void SetPosition(const Eigen::Vector3d &position){
-        std::unique_lock<std::mutex> lck(update_get_mutex_);
+        std::unique_lock<std::mutex> lck(mutex_pos_);
         Eigen::Vector3d* base = this; 
         *base = position;
     }
@@ -27,7 +27,7 @@ public:
     int observed_times_ = 0;
     bool is_outlier_ = false;
 private:
-    std::mutex update_get_mutex_;
+    std::mutex mutex_pos_;
     // std::list<std::weak_ptr<Feature>> active_observations_;
     // std::list<std::weak_ptr<Feature>> observations_;
 };
