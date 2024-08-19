e196b47c107688cf1e31f3b78f11a73a5ab610f3
FuYi <1766423721@qq.com>
2024-08-19 18:07:55 +0800

    sensor_msgs cv_bridge

--- ros/slam.cpp
+++ ros/slam.cpp
@@ -3,6 +3,9 @@
 #include <rclcpp/node.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+#include <chrono>
 #include "M_VSLAM/frontend.h"
 class Sub : public rclcpp::Node
 {
@@ -11,6 +14,10 @@ public:
     : Node("slam"){
         subscription_ = this->create_subscription<std_msgs::msg::String>(
         "topic", 10, std::bind(&Sub::topic_callback, this, std::placeholders::_1));
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(500), std::bind(&Sub::timer_callback, this)
+        );
     }
 
 private:
@@ -18,6 +25,15 @@ private:
         RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    void timer_callback(){
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
 };
 
 int main(int argc,char** argv)
